549e3d0e5ac25858dba66c2ec0a96e6f677bfcc7
Jurij Ambros <jurij.ambros@gmail.com>
2021-04-15 18:16:44 +0300

    Prijungtas entity framework

---
+++ 3PL1_Biblioteka/Domain/BibliotekaDbContext.cs
@@ -0,0 +1,29 @@
+﻿using MySql.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+	public class BibliotekaDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+		}
+
+		public DbSet<Kategorija> Kategorijos { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Kategorija>(entity => {
+				entity.Property(m => m.AmžiausCenzūra).HasColumnName("AmziausCenzura").HasMaxLength(50);
+				entity.HasKey(m => m.Id);
+				entity.Property(m => m.Id).ValueGeneratedOnAdd();
+			});
+		}
+	}
+}
